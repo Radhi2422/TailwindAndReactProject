246588d0264a37c88cc9ef26ea0dffb2b0c5ad4a
Radhi2422 <106824237+Radhi2422@users.noreply.github.com>
2025-04-28 13:28:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Hero } from './assets/Hero'
+import { NavBar } from './assets/NavBar'
+
+function App() {
+
+  return (
+    <>
+    <NavBar/>
+    <Hero/>
+    </>
+    
+  )
+}
+
+export default App
